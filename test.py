447d228ea3ae9f5780b0366ed25c0b6b7b05f3cc
SertugF <sertugfidan@outlook.com>
2023-02-11 00:16:59 +0300

    pi da denemek için son versiyon

--- test.py
+++ test.py
@@ -1,17 +1,42 @@
-# NIRScanner Nano Python wrapper.
-# Created by Weiwei Jiang on 2018/06/01
-#
+# en son pi versiyonu test için oluşturuldu.
 
 import os
 import sys
 from NIRS import NIRS
 
+
+
 def main():
+    print("Versiyon: ")
     nirs = NIRS()
-    print("Scanning...")
-    nirs.scan()
-    results = nirs.get_scan_results()
-    print(results)
+    nirs.display_version()
+
 
 if __name__ == "__main__":
     main()
+
+
+
+
+ # scan
+ # nirs = NIRS()
+    # print("Scanning...")
+    # nirs.scan()
+    # results = nirs.get_scan_results()
+    # print(results)
+
+
+# save as csv, does it need file exist check ???
+#     with open("results.csv", "w") as f:
+#         f.write("wavelength,intensity,reference")
+#         for i in range(results["valid_length"]):
+#             f.write("{},{},{}n#".format(results["wavelength"][i], results["intensity"][i], results["reference"][i]))
+
+#     print("Done.")
+
+
+
+
+
+
+
